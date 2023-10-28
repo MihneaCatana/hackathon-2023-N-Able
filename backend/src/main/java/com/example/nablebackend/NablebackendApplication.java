421fcac4cd648d118ebf9cba5a4594b4b75f221c
MarioBujor2001 <bujormario2001@gmail.com>
2023-10-28 09:02:04 +0300

    added backend

---
+++ backend/src/main/java/com/example/nablebackend/NablebackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.nablebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NablebackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NablebackendApplication.class, args);
+    }
+
+}
