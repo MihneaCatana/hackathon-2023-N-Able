e473a802e8493854ad41dd1ba6085e8c5fc3367a
MILET <mihneacatana@gmail.com>
2023-10-28 18:11:08 +0300

    implemented tabs in Homepage

--- frontend/hackathon/src/pages/Events/Events.tsx
+++ frontend/hackathon/src/pages/Events/Events.tsx
@@ -2,9 +2,11 @@ import Navbar from "../../components/Navbar/Navbar.tsx";
 
 const Events = () => {
 
+    const city = localStorage.getItem('defaultCity');
+
     return <>
         <Navbar/>
-        
+        <h2>Your current city: {city}</h2>
     </>
 }
 
--- frontend/hackathon/src/pages/Homepage/Homepage.tsx
+++ frontend/hackathon/src/pages/Homepage/Homepage.tsx
@@ -1,9 +1,14 @@
+import {useEffect, useState} from "react";
+
+import HomepageCard from "../../components/HomepageCard/HomepageCard.tsx";
 import Navbar from "../../components/Navbar/Navbar.tsx";
 
-import "./Homepage.css"
-import {useEffect, useState} from "react";
+import {TabPanel, TabView} from "primereact/tabview";
+
 import Axios from "axios";
-import HomepageCard from "../../components/HomepageCard/HomepageCard.tsx";
+
+import "./Homepage.css"
+
 
 type EventType = {
     user: {
@@ -23,28 +28,87 @@ type EventType = {
 const Homepage = () => {
 
     const [eventsList, setEventsList] = useState([])
+    const [tabValue, setTabValue] = useState<number>(0);
+    const arrayCategories = ['', 'Cultural', 'Outdoors', 'Sport', 'Concert']
 
     useEffect(() => {
-        Axios.get('http://localhost:8080/events').then((res) => {
+        Axios.get(`http://localhost:8080/events${tabValue != 0 ? '?tag=' + arrayCategories[tabValue] : ''}`).then((res) => {
             console.log(res.data)
             setEventsList(res.data)
+        }).catch(() => {
+            setEventsList([])
         })
-    }, [])
+    }, [tabValue])
 
 
     return <>
         <div className='homepage_container'>
             <Navbar/>
-            <h1 style={{textAlign: 'center'}}>Sponsored Content</h1>
-            <div className='homepage_cards_section'>
-                {eventsList.map((elem: EventType) => <HomepageCard description={elem.event.description}
-                                                                   photo_url={elem.event.photoUrl}
-                                                                   title={elem.event.title}
-                                                                   location={elem.event.location}
-                                                                   date={elem.event.date}
-                                                                   ticket_price={elem.event.ticketPrice}
-                                                                   key={elem.event.id}/>)}
-            </div>
+            <TabView activeIndex={tabValue} onTabChange={(event) => setTabValue(event.index)}
+                     style={{marginTop: '0rem'}}>
+                <TabPanel header="Sponsored Content">
+                    <h1 style={{textAlign: 'center'}}>Sponsored Content</h1>
+                    <div className='homepage_cards_section'>
+                        {eventsList.map((elem: EventType) => <HomepageCard description={elem.event.description}
+                                                                           photo_url={elem.event.photoUrl}
+                                                                           title={elem.event.title}
+                                                                           location={elem.event.location}
+                                                                           date={elem.event.date}
+                                                                           ticket_price={elem.event.ticketPrice}
+                                                                           key={elem.event.id}/>)}
+                    </div>
+                </TabPanel>
+                <TabPanel header="Cultural">
+                    <h1 style={{textAlign: 'center'}}>Cultural</h1>
+                    <div className='homepage_cards_section'>
+                        {eventsList.map((elem: EventType) => <HomepageCard description={elem.event.description}
+                                                                           photo_url={elem.event.photoUrl}
+                                                                           title={elem.event.title}
+                                                                           location={elem.event.location}
+                                                                           date={elem.event.date}
+                                                                           ticket_price={elem.event.ticketPrice}
+                                                                           key={elem.event.id}/>)}
+                    </div>
+                </TabPanel>
+                <TabPanel header="Outdoors">
+                    <h1 style={{textAlign: 'center'}}>Outdoors</h1>
+                    <div className='homepage_cards_section'>
+                        {eventsList.map((elem: EventType) => <HomepageCard description={elem.event.description}
+                                                                           photo_url={elem.event.photoUrl}
+                                                                           title={elem.event.title}
+                                                                           location={elem.event.location}
+                                                                           date={elem.event.date}
+                                                                           ticket_price={elem.event.ticketPrice}
+                                                                           key={elem.event.id}/>)}
+                    </div>
+                </TabPanel>
+                <TabPanel header="Sport">
+                    <h1 style={{textAlign: 'center'}}>Sport</h1>
+                    <div className='homepage_cards_section'>
+                        {eventsList.map((elem: EventType) => <HomepageCard description={elem.event.description}
+                                                                           photo_url={elem.event.photoUrl}
+                                                                           title={elem.event.title}
+                                                                           location={elem.event.location}
+                                                                           date={elem.event.date}
+                                                                           ticket_price={elem.event.ticketPrice}
+                                                                           key={elem.event.id}/>)}
+                    </div>
+                </TabPanel>
+                <TabPanel header="Concert">
+                    <h1 style={{textAlign: 'center'}}>Concert</h1>
+                    <div className='homepage_cards_section'>
+                        {eventsList.length ? eventsList.map((elem: EventType) => <HomepageCard
+                            description={elem.event.description}
+                            photo_url={elem.event.photoUrl}
+                            title={elem.event.title}
+                            location={elem.event.location}
+                            date={elem.event.date}
+                            ticket_price={elem.event.ticketPrice}
+                            key={elem.event.id}/>) : <p> There are no events. Please come back later!</p>}
+                    </div>
+                </TabPanel>
+            </TabView>
+
         </div>
     </>
 }
